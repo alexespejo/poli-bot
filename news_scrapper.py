bb727d0eb229c2899de24e78e099273d82263dd4
Alex Espejo <aespejo102@gmail.com>
2024-04-19 21:50:24 -0700

    scraping

--- news_scrapper.py
+++ news_scrapper.py
@@ -1,6 +1,20 @@
 import newspaper
-from data import urls
-cnn_paper = newspaper.build(urls[0])
+# from data import urls
+# cnn_paper = newspaper.build("http://washingtonindependent.com")
+# 
+# for article in cnn_paper.articles:
+#     print(article.url)
 
-for article in cnn_paper.articles:
-    print(article.url)
+article = newspaper.Article("https://www.cnn.com/politics/live-news/trump-hush-money-trial-04-15-24/index.html")
+
+article.download()
+article.parse()
+
+# article text
+print(article.text)
+
+# article authors
+print(article.authors)
+
+#article publish date
+print(article.publish_date)
